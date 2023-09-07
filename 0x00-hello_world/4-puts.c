08b142442e947ffe806cb215974859a54d1ee578
Machixter <tuqamachi7@gmail.com>
2023-09-07 19:43:14 +0300

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - this is main function
+ *
+ * Return: returns 0 if successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
