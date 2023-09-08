a750946b15a869886818685e22c0fcfaea8c3cab
Machixter <tuqamachi7@gmail.com>
2023-09-08 20:48:49 +0300

    The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
 /**
- * main - this is the main function 
+ * main - this is the main function
  *
  * Return: is always 0
  */
 int main(void)
 {
+	int x, y;
 
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		for (y = x + 1 ; y <= 9 ; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (x != 8 || y != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
