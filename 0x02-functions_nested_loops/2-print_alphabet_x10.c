2950bde5b66ae33b4077ea38385b3ccd37ce35a1
Machixter <tuqamachi7@gmail.com>
2023-09-13 00:13:19 +0300

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets 10x
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	char y;
+
+	for (x = 0 ; x < 10 ; x++)
+	{
+		for (y = 'a' ; y <= 'z' ; y++)
+			_putchar(y);
+		_putchar('\n');
+	}
+	
+}
